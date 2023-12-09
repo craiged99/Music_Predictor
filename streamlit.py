657b087ece421658b64e9ee8000e276c7c83f0db
craiged99 <97758923+craiged99@users.noreply.github.com>
2023-12-09 21:26:09 +0000

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os, sys
 
-@st.cache_resource
+@st.cache
 def installff():
   os.system('sbase install geckodriver')
   os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
